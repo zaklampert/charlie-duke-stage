f19d48f4cd222962356865a3abe519700330321d
Andrew Cafourek <andrew@a022digital.com>
2017-03-29 09:17:51 -0400

    Add Apollo16 theme

---
+++ wp-content/themes/apollo16/index.php
@@ -0,0 +1,27 @@
+
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Charlie Duke</title>
+	<meta name="title" content="Charlie Duke" />
+	<meta name="description" content="Charlie Duke." />
+	<meta name="a022" content="WPE" />
+
+
+  <style>
+    html {
+			background: url('<?php echo get_stylesheet_directory_uri();?>/images/duke.jpg') no-repeat center center fixed;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+    }
+		</style>
+</head>
+<body>
+
+</body>
+
+
+</html>
