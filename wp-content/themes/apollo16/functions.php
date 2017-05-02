c6f4f3debe8cbf01a6d6f3ca190105b084dd2668
Andrew Cafourek <andrew@a022digital.com>
2017-05-02 17:28:45 -0400

    update header requests

--- wp-content/themes/apollo16/functions.php
+++ wp-content/themes/apollo16/functions.php
@@ -84,3 +84,18 @@ function duke_create_post_types() {
 		}
 		return $results;
 	}
+
+	/**
+		 * Only allow GET requests and add cross origin wildcard
+		 */
+		add_action( 'rest_api_init', function() {
+
+			remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+			add_filter( 'rest_pre_serve_request', function( $value ) {
+				header( 'Access-Control-Allow-Origin: *');
+				header( 'Access-Control-Allow-Methods: GET' );
+
+				return $value;
+
+			});
+		}, 15 );
